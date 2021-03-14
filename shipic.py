a55a40f3b489225692625e8a187e3fb83c0f9b5a
shiwning <79551246+shiwning@users.noreply.github.com>
2021-03-14 08:31:12 +0800

    Add files via upload
    二稿图像修正（by swn）

--- shipic.py
+++ shipic.py
@@ -1,134 +1,99 @@
 import numpy as np
-import copy
+import argparse
 import cv2
+#ap=argparse.ArgumentParser()
+#ap.add_argument("-i", "--image", required = True, help = "path to the image file")
+#args = vars(ap.parse_args())
+def zhaopian(file_path):
+    image = cv2.imread(file_path)#读入图片
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)#转成灰度图
 
+    #接下来使图片二值化，为了减小误差
+    gradX = cv2.Sobel(gray, ddepth = cv2.CV_32F, dx = 1, dy = 0, ksize = -1)
+    gradY = cv2.Sobel(gray, ddepth = cv2.CV_32F, dx = 0, dy = 1, ksize = -1)
+    #图片锐利化对比度什么的
+    gradient = cv2.subtract(gradX, gradY)
+    gradient = cv2.convertScaleAbs(gradient)
 
-def reshape_image(image):
-    '''归一化图片尺寸：短边400，长边不超过800，短边400，长边超过800以长边800为主'''
-    width, height = image.shape[1], image.shape[0]
-    min_len = width
-    scale = width * 1.0 / 400
-    new_width = 400
-    new_height = int(height / scale)
-    if new_height > 800:
-        new_height = 800
-        scale = height * 1.0 / 800
-        new_width = int(width / scale)
-    out = cv2.resize(image, (new_width, new_height))
-    return out
+    #cv2.imshow("gradient",gradient)
+    #原本没有过滤颜色通道的时候，这个高斯模糊有效，但是如果进行了颜色过滤，不用高斯模糊效果更好
+    #blurred = cv2.blur(gradient, (9, 9))
+    #二值化保存到thresh
+    (_, thresh) = cv2.threshold(gradient, 225, 255, cv2.THRESH_BINARY)
+    #cv2.imshow("thresh",thresh)
+    #cv2.imwrite('thresh.jpg',thresh)
 
+    #寻找最小外接矩形
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 21))
+    closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+    #cv2.imshow("closed",closed)
+    #cv2.imwrite('closed.jpg',closed)
+    #膨胀与腐蚀，这个作用是提高对比度  另外，此处方法不唯一。。。。这个方法是我在网上搜的，博主倾力推荐。。
+    closed = cv2.erode(closed, None, iterations = 4)
+    closed = cv2.dilate(closed, None, iterations = 4)
+    #cv2.imwrite('closed1.jpg',closed)
+    #确定包含二维码的矩形位置
+    cnts, _ = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
 
-def detecte(image):
-    '''提取所有轮廓'''
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, gray = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
-    contours, hierachy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    return image, contours, hierachy
+    rect = cv2.minAreaRect(c)
+    box = np.int0(cv2.boxPoints(rect))
 
+    cv2.drawContours(image, [box], -1, (0, 255, 0), 3)
+    #cv2.imwrite("final.jpg",image)
+    #cv2.imshow("Image", image)
+    #剪裁矩形，用类似切片的操作只能减矩形
+    left_point_x = np.min(box[:, 0])
+    right_point_x = np.max(box[:, 0])
+    top_point_y = np.min(box[:, 1])
+    bottom_point_y = np.max(box[:, 1])
+    ans = image[top_point_y:bottom_point_y,left_point_x:right_point_x]
+    #cv2.imshow("ans",ans)
+    #cv2.imwrite("ans.jpg",ans)
+    return ans
 
-def compute_1(contours, i, j):
-    '''最外面的轮廓和子轮廓的比例'''
-    area1 = cv2.contourArea(contours[i])
-    area2 = cv2.contourArea(contours[j])
-    if area2 == 0:
-        return False
-    ratio = area1 * 1.0 / area2
-    if abs(ratio - 49.0 / 25):
-        return True
-    return False
+def imgToSize(img):
+    ''' imgToSize()
+    # ----------------------------------------
+    # Function:   将图像等比例缩放到 512x512 大小
+    #             根据图像长宽不同分为两种缩放方式
+    # Param img:  图像 Mat
+    # Return img: 返回缩放后的图片
+    # Example:    img = imgToSize(img)
+    # ----------------------------------------
+    '''
+    # 测试点
+    # cv2.imshow('metaImg.jpg', img)
 
+    imgHeight, imgWidth = img.shape[:2]
 
-def compute_2(contours, i, j):
-    '''子轮廓和子子轮廓的比例'''
-    area1 = cv2.contourArea(contours[i])
-    area2 = cv2.contourArea(contours[j])
-    if area2 == 0:
-        return False
-    ratio = area1 * 1.0 / area2
-    if abs(ratio - 25.0 / 9):
-        return True
-    return False
+    # cv.resize(src, dsize[, dst[, fx[, fy[, interpolation]]]])
+    # src 原图像，dsize 输出图像的大小，
+    # img = cv2.resize(img, (512,512))
+    zoomHeight = 512
+    zoomWidth = int(imgWidth*512/imgHeight)
+    img = cv2.resize(img, (zoomWidth,zoomHeight))
 
+    # 测试点
+    # cv2.imshow('resizeImg', img)
 
-def compute_center(contours, i):
-    '''计算轮廓中心点'''
-    M = cv2.moments(contours[i])
-    cx = int(M['m10'] / M['m00'])
-    cy = int(M['m01'] / M['m00'])
-    return cx, cy
-
-
-def detect_contours(vec):
-    '''判断这个轮廓和它的子轮廓以及子子轮廓的中心的间距是否足够小'''
-    distance_1 = np.sqrt((vec[0] - vec[2]) ** 2 + (vec[1] - vec[3]) ** 2)
-    distance_2 = np.sqrt((vec[0] - vec[4]) ** 2 + (vec[1] - vec[5]) ** 2)
-    distance_3 = np.sqrt((vec[2] - vec[4]) ** 2 + (vec[3] - vec[5]) ** 2)
-    if sum((distance_1, distance_2, distance_3)) / 3 < 3:
-        return True
-    return False
-
-
-def juge_angle(rec):
-    '''判断寻找是否有三个点可以围成等腰直角三角形'''
-    if len(rec) < 3:
-        return -1, -1, -1
-    for i in range(len(rec)):
-        for j in range(i + 1, len(rec)):
-            for k in range(j + 1, len(rec)):
-                distance_1 = np.sqrt((rec[i][0] - rec[j][0]) ** 2 + (rec[i][1] - rec[j][1]) ** 2)
-                distance_2 = np.sqrt((rec[i][0] - rec[k][0]) ** 2 + (rec[i][1] - rec[k][1]) ** 2)
-                distance_3 = np.sqrt((rec[j][0] - rec[k][0]) ** 2 + (rec[j][1] - rec[k][1]) ** 2)
-                if abs(distance_1 - distance_2) < 5:
-                    if abs(np.sqrt(np.square(distance_1) + np.square(distance_2)) - distance_3) < 5:
-                        return i, j, k
-                elif abs(distance_1 - distance_3) < 5:
-                    if abs(np.sqrt(np.square(distance_1) + np.square(distance_3)) - distance_2) < 5:
-                        return i, j, k
-                elif abs(distance_2 - distance_3) < 5:
-                    if abs(np.sqrt(np.square(distance_2) + np.square(distance_3)) - distance_1) < 5:
-                        return i, j, k
-    return -1, -1, -1
-
-
-def find(image, contours, hierachy, root=0):
-    '''找到符合要求的轮廓'''
-    rec = []
-    for i in range(len(hierachy)):
-        child = hierachy[i][2]
-        child_child = hierachy[child][2]
-        if child != -1 and hierachy[child][2] != -1:
-            if compute_1(contours, i, child) and compute_2(contours, child, child_child):
-                cx1, cy1 = compute_center(contours, i)
-                cx2, cy2 = compute_center(contours, child)
-                cx3, cy3 = compute_center(contours, child_child)
-                if detect_contours([cx1, cy1, cx2, cy2, cx3, cy3]):
-                    rec.append([cx1, cy1, cx2, cy2, cx3, cy3, i, child, child_child])
-    '''计算得到所有在比例上符合要求的轮廓中心点'''
-    i, j, k = juge_angle(rec)
-    if i == -1 or j == -1 or k == -1:
-        return
-    ts = np.concatenate((contours[rec[i][6]], contours[rec[j][6]], contours[rec[k][6]]))
-    rect = cv2.minAreaRect(ts)
-    box = cv2.boxPoints(rect)
-    box = np.int0(box)
-    result = copy.deepcopy(image)
-    # cv2.drawContours(result, [box], 0, (0, 0, 255), 2)
-    # cv2.drawContours(image, contours, rec[i][6], (255, 0, 0), 2)
-    # cv2.drawContours(image, contours, rec[j][6], (255, 0, 0), 2)
-    # cv2.drawContours(image, contours, rec[k][6], (255, 0, 0), 2)
-    # cv2.imshow('img', image)
-    # cv2.waitKey(0)
-    #cv2.imshow('img', result)
-    #cv2.waitKey(0)
-
-    return result
-
-
-def pictu(image):
-    #image = cv2.imread(str(imgpath))
-    #cv2.imshow('ss',image)
-    #cv2.waitKey(0)
-    image = reshape_image(image)
-    image, contours, hierachy = detecte(image)
-    ansimg = find(image, contours, np.squeeze(hierachy))
-    return ansimg
+    # 如果图片属于 Width<Height，那么宽度将达不到 512
+    if imgWidth >= imgHeight:
+        # 正常截取图像
+        w1 = (zoomWidth-512)//2
+        # 图像坐标为先 Height，后 Width
+        img = img[0:512, w1:w1+512]
+    else:
+        # 如果宽度小于 512，那么对两侧边界填充为全黑色
+        # 根据图像的边界的像素值，向外扩充图片，每个方向扩充50个像素，常数填充：
+        # dst = cv2.copyMakeBorder(src, top, bottom, left, right, borderType[, dst[, value]])
+        # dst = cv2.copyMakeBorder(img,50,50,50,50, cv2.BORDER_CONSTANT,value=[0,255,0])
+        # 需要填充的宽度为 512-zoomWidth
+        left = (512-zoomWidth)//2
+        # 避免余数取不到
+        right = left+1
+        img = cv2.copyMakeBorder(img, 0,0,left,right, cv2.BORDER_CONSTANT, value=[0,0,0])
+        img = img[0:512, 0:512]
+    # 测试点
+    # cv2.imshow('size512', img)
+    return img
