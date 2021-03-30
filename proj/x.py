779fde1726c9c99a5268743359582490d41b008b
likefengzi <78586025+likefengzi@users.noreply.github.com>
2021-03-30 20:24:32 +0800

    270*270
    270*270

--- proj/x.py
+++ proj/x.py
@@ -1,9 +1,11 @@
 border = 4
-width = 93
+width = 270
 inWidth = 41
-locWidth = 16
-blackWhite = 14
-sLocWidth = 8
-sblackWhite = 7
+locWidth = 25
+blackWhite = 23
+sLocWidth = 18
+sblackWhite = 16
 cutlength = 0
 key = '1011'
+
+
